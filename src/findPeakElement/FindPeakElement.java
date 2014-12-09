851019cceda920d17986b82b7a29ecad03c86cbc
hanliang <417799142@qq.com>
2014-12-09 17:19:32 +0800

    修改排版

--- src/findPeakElement/FindPeakElement.java
+++ src/findPeakElement/FindPeakElement.java
@@ -6,32 +6,30 @@ public class FindPeakElement {
 		System.out.println(fpe.findPeakElement(num));
 	}
 	
-	 public int findPeakElement(int[] num) {
-		 int size = num.length;
-		 if(size==1)return 0;
-		 if(num[0]>num[1])return 0;
-		 if(num[size-1]>num[size-2])return size-1;
-		 return binarySerch(0,size-1,num);
-	        
+	public int findPeakElement(int[] num) {
+		int size = num.length;
+		if(size==1)return 0;
+		if(num[0]>num[1])return 0;
+		if(num[size-1]>num[size-2])return size-1;
+		return binarySerch(0,size-1,num);       
 	 }
 	 
-	 public int binarySerch(int start,int end,int[] arr){
-		 int mid = (start+end)/2;
-		 if(mid == start||mid == end){
-			 if(arr[start]>arr[end]){
-				 return start;
-			 }else{
-				 return end;
-			 }
-		 }
-		 if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){
-			 return mid;
-		 }else if(arr[mid]<arr[mid-1]){
-			 return binarySerch(start,mid-1,arr);
-		 }else if(arr[mid]<arr[mid+1]){
-			 return binarySerch(mid+1,end,arr);
-		 }
-		 return 0;
-	 }
-
+	public int binarySerch(int start,int end,int[] arr){
+		int mid = (start+end)/2;
+		if(mid == start||mid == end){
+			if(arr[start]>arr[end]){
+				return start;
+			}else{
+				return end;
+			}
+		}
+		if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){
+			return mid;
+		}else if(arr[mid]<arr[mid-1]){
+			return binarySerch(start,mid-1,arr);
+		}else if(arr[mid]<arr[mid+1]){
+			return binarySerch(mid+1,end,arr);
+		}
+		return 0;
+	}
 }
